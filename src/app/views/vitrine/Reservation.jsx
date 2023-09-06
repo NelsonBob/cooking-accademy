861ce007a2283bfbc48505bf3270173c1ef74d0f
NelsonBob <nfomekongnoagni@myges.fr>
2023-09-06 11:54:17 +0200

    integration de l'interface de reservation

--- src/app/views/vitrine/Reservation.jsx
+++ src/app/views/vitrine/Reservation.jsx
@@ -1,39 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import moment from "moment";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useNavigate } from "react-router-dom";
 import {
   Button,
+  Card,
   Col,
   Container,
   Modal,
-  ModalBody,
-  ModalHeader,
   Row,
   UncontrolledCarousel,
 } from "reactstrap";
 import { getSalleById, readFile } from "../../service/frontendService";
+import Swal from "sweetalert2";
+const localizer = momentLocalizer(moment);
 
 const Reservation = () => {
-  const { t } = useTranslation();
   const [tableData, setTableData] = useState({});
   const [imageUrls, setImageUrls] = useState([]);
+  const [reserve, setReserve] = useState(false);
   const idSalle = localStorage.getItem("idSalle");
-  const [modalOpen, setModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {}, [imageUrls]);
   useEffect(() => {
     getSalle();
   }, []);
-  const openModal = (imag) => {
-    setSelectedImage(imag);
-    setModalOpen(true);
-  };
 
-  const closeModal = () => {
-    setSelectedImage(null);
-    setModalOpen(false);
-  };
   const getSalle = async () => {
     try {
       const res = await getSalleById(idSalle);
@@ -59,6 +52,101 @@ const Reservation = () => {
       console.error("Error displaying file:", error);
     }
   };
+
+  const [myEvents, setEvents] = useState([
+    {
+      id: 0,
+      title: "All Day Event very long title",
+      allDay: true,
+      start: new Date(2023, 9, 0),
+      end: new Date(2023, 9, 1),
+    },
+  ]);
+
+  const { defaultDate } = useMemo(
+    () => ({
+      defaultDate: new Date(),
+    }),
+    []
+  );
+  const handleSelectEvent = useCallback(
+    (event) => {
+      const formattedStartTime = moment(event.start).format("LT");
+      const formattedEndTime = moment(event.end).format("LT");
+      const formattedDay = moment(event.start).format("LL");
+      Swal.fire({
+        html: `<h2>Remove Event at ${formattedDay} </h2> Are you sure you want to remove the event: 
+        <strong>${event.title}</strong>(<strong>${formattedStartTime}</strong> - <strong>${formattedEndTime}</strong>)?`,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, remove it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // User confirmed the removal, remove the event from myEvents
+          const updatedEvents = myEvents.filter(
+            (e) =>
+              e.start !== event.start &&
+              e.end !== event.end &&
+              e.title !== event.title
+          );
+          setEvents(updatedEvents);
+          Swal.fire("Event Removed!", "The event has been removed.", "success");
+        }
+      });
+    },
+    [myEvents, setEvents]
+  );
+
+  const handleSelectSlot = useCallback(
+    ({ start, end }) => {
+      // Check for conflicts with existing events
+      const isConflict = myEvents.some((event) => {
+        return (
+          (start >= event.start && start < event.end) ||
+          (end > event.start && end <= event.end) ||
+          (start <= event.start && end >= event.end)
+        );
+      });
+
+      if (isConflict) {
+        Swal.fire({
+          position: "top-end",
+          icon: "error",
+          html: `
+          <p>
+            This time slot conflicts with an existing event. Please choose a
+            different time.
+          </p>
+        `,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else {
+        const formattedStartTime = moment(start).format("LT");
+        const formattedEndTime = moment(end).format("LT");
+        const formattedDay = moment(start).format("LL");
+
+        Swal.fire({
+          html: `<h2>Reservation du ${formattedDay}</h2>
+          <span>${formattedStartTime} - ${formattedEndTime}</span>`,
+          input: "text",
+          inputLabel: "Entrer un intutilé de reservation",
+          showCancelButton: true,
+          inputValidator: (value) => {
+            if (!value) {
+              return "You need to write something!";
+            } else {
+              setEvents((prev) => [...prev, { start, end, title: value }]);
+            }
+          },
+        });
+      }
+    },
+    [myEvents, setEvents]
+  );
+
   if (!idSalle) return navigate("/out/location");
   else
     return (
@@ -101,33 +189,42 @@ const Reservation = () => {
                   md={6}
                   className="my-4 d-flex justify-content-end align-items-baseline"
                 >
-                  <Button color="primary">Réserver maintenant</Button>{" "}
+                  <Button color="primary" onClick={() => setReserve(!reserve)}>
+                    {reserve ? "Retour" : "Réserver maintenant"}
+                  </Button>{" "}
                 </Col>
-                <Col md={12}>
-                  <UncontrolledCarousel items={imageUrls} />
-                </Col>
-                <Col md={12}>
-                  <h2>Description</h2>
-                  <p>{tableData.description}</p>
-                </Col>
+                {!reserve ? (
+                  <>
+                    <Col md={12}>
+                      <UncontrolledCarousel items={imageUrls} className="reservation" />
+                    </Col>
+                    <Col md={12}>
+                      <h2>Description</h2>
+                      <p>{tableData.description}</p>
+                    </Col>
+                  </>
+                ) : (
+                  <Col lg={12}>
+                    <div>
+                      <h1>Reservation System</h1>
+                      <Card className="p-4">
+                        <Calendar
+                          defaultDate={defaultDate}
+                          defaultView="day"
+                          events={myEvents}
+                          localizer={localizer}
+                          onSelectEvent={handleSelectEvent}
+                          onSelectSlot={handleSelectSlot}
+                          views={["day"]}
+                          selectable
+                        />
+                      </Card>
+                    </div>
+                  </Col>
+                )}
               </>
             )}
           </Row>
-          <Modal isOpen={modalOpen} toggle={closeModal}>
-            <ModalHeader toggle={closeModal}>Image Preview</ModalHeader>
-            <ModalBody>
-              {selectedImage && (
-                <img
-                  src={selectedImage}
-                  alt="..."
-                  style={{ maxWidth: "100%" }}
-                />
-              )}
-            </ModalBody>
-            <Button color="secondary" onClick={closeModal}>
-              Close
-            </Button>
-          </Modal>
         </Container>
       </>
     );
