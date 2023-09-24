55265c73049c97f32bcb82e62f7e8ec1835ad723
NelsonBob <91746270+NelsonBob@users.noreply.github.com>
2023-09-24 15:47:56 +0200

    config netlify

--- src/environnements/environnement.js
+++ src/environnements/environnement.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://localhost:8079'
-// export const baseURL ='https://pa-backend.com';
+// export const baseURL = 'http://localhost:8079'
+export const baseURL ='https://cooking-backend.selbitech.com';
